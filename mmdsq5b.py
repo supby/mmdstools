7af6d1e2ff63ba79c4bd988f5926cc7ea169bf9c
Andre_Suzanovich <Andre_Suzanovich@epam.com>
2014-11-06 18:16:44 +0300

    clustering

--- mmdsq5b.py
+++ mmdsq5b.py
@@ -8,18 +8,13 @@ def calcDistance(p1, p2):
 def calcClusterMeanPoint(points):
 	length = len(points)
 	return (float(sum([p[0] for p in points]))/length, float(sum([p[1] for p in points]))/length)
-	
-if __name__ == '__main__':
 
-	initialCentriods = [(25,125), (44,105), (29,97), (35,63), (55,63), (42,57), (23,40), (64,37), (33,22), (55,20)]
-	initialPoints = [(28,145),(65,140),(50,130),(38,115),(55,118),(50,90),(43,83),(63,88),(50,60),(50,30)]
+def assignPoints2Clusters(centriods, points):
 	cluster2Points = {}
-
-	# initial assign
-	for p in initialPoints:
+	for p in points:
 		minD = -1
 		closestC = None
-		for c in initialCentriods:
+		for c in centriods:
 			curD = calcDistance(p, c)
 			if minD == -1 or curD < minD:
 				minD = curD
@@ -28,7 +23,29 @@ if __name__ == '__main__':
 		arr.append(p)
 		cluster2Points[closestC] = arr
 
+	return cluster2Points
+	
+if __name__ == '__main__':
+
+	centriods = [(25,125), (44,105), (29,97), (35,63), (55,63), (42,57), (23,40), (64,37), (33,22), (55,20)]
+	points = [(28,145),(65,140),(50,130),(38,115),(55,118),(50,90),(43,83),(63,88),(50,60),(50,30)]	
+
+	# initial assign	
+	cluster2Points = assignPoints2Clusters(centriods, points)
+	print "0st assign (initial):"
 	print cluster2Points
+
+	# recalculate means for clusters	
+	centriods = []
+	for k,v in cluster2Points.items():
+		meanPoint = calcClusterMeanPoint(v)
+		centriods.append(meanPoint)
+
+	# second assign	
+	cluster2Points = assignPoints2Clusters(centriods, points)
+	print "1st assign:"
+	print cluster2Points	
+
 
 
 	
